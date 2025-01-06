3c1d0dc389d4ed2e25b1f167d00de027286f2dc4
Pamade <--global>
2025-01-06 21:47:23 +0100

    update styling side section

--- src/SideNavigation/SideNavigation.tsx
+++ src/SideNavigation/SideNavigation.tsx
@@ -1,12 +1,12 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, useParams, Link } from "react-router-dom"
 import styles from "./SideNavigation.module.scss";
 import { IoCloseCircleOutline } from "react-icons/io5";
-import { TbChartBarPopular } from "react-icons/tb";
-import { IconBaseProps, IconType } from "react-icons";
+import { IconType } from "react-icons";
 import { MdSportsFootball } from "react-icons/md";
 import { FaHandsHelping } from "react-icons/fa";
 import { MdScience } from "react-icons/md";
 import { AiFillEnvironment } from "react-icons/ai";
+import { IoHome } from "react-icons/io5";
 
 
 
@@ -20,30 +20,31 @@ const sections = [{type:"sport", icon:MdSportsFootball},
      {type:"politics", icon:FaHandsHelping }, {type:"science", icon:MdScience}, {type:"environment", icon:AiFillEnvironment}] as const;
 
 function SideNavigation({isNavigationOpen, setIsNavigationOpen}:Props) {
-    console.log(isNavigationOpen)
     return (
             <nav className={!isNavigationOpen ? styles.navigation : `${styles.navigation} ${styles.navigation_open}`}>
                     <div className={styles.close_container}>
                         <IoCloseCircleOutline className={styles.close_button} onClick={() => {typeof setIsNavigationOpen !== "undefined" && setIsNavigationOpen(false)}}/>
-                    </div>   
-                    <h4  className={styles.heading}><NavLink to="/">Home</NavLink></h4>
-                    <div>
-                        <ul className={styles.list}>
-                            {sections.map(({type, icon}) => <NavigationLink Icon={icon} text={type}/>)}
-                            {/* <NavigationLink Icon={TbChartBarPopular} path="/" text="Popular"/>
-                            <NavLink to="/"><div className={styles.item_content}> <TbChartBarPopular />Popular</div></NavLink>
-                            <NavLink to="/">New</NavLink>
-                            <NavLink to="/">Poland</NavLink> */}
-                        </ul>
                     </div>
-                    <div>
-                    <h4  className={styles.heading}><NavLink to="/commentisfree">Opinions</NavLink></h4>
-                        <ul className={styles.list}>
-                            <NavLink to="/">Author 1</NavLink>
-                            <NavLink to="/">Author 1</NavLink>
-                        </ul>
-                    </div>
-                
+                    <section className={styles.content}>
+                        <Link className={styles.heading_home_link} to="/"><h4 className={`${styles.heading} ${styles.heading_home}`}><IoHome className={styles.icon}/> Home</h4></Link>
+                        <div>
+                            <h4 className={styles.heading}>Popular Topics</h4>
+                            <ul className={styles.list}>
+                                {sections.map(({type, icon}) => <NavigationLink Icon={icon} text={type}/>)}
+                                {/* <NavigationLink Icon={TbChartBarPopular} path="/" text="Popular"/>
+                                <NavLink to="/"><div className={styles.item_content}> <TbChartBarPopular />Popular</div></NavLink>
+                                <NavLink to="/">New</NavLink>
+                                <NavLink to="/">Poland</NavLink> */}
+                            </ul>
+                        </div>
+                        <div>
+                        <h4 className={styles.heading}><NavLink to="/commentisfree">Opinions</NavLink></h4>
+                            <ul className={styles.list}>
+                                <NavLink to="/">Author 1</NavLink>
+                                <NavLink to="/">Author 1</NavLink>
+                            </ul>
+                        </div>
+                    </section>   
             </nav>
     )
 }
@@ -53,7 +54,8 @@ interface NavigationLinkProps{
 }
 
 function NavigationLink({ Icon, text}:NavigationLinkProps) {
-
-    return <NavLink to={text}><div className={styles.item_content}>{Icon && <Icon className={styles.icon} />}{text}</div></NavLink>
+    const {sectionID} = useParams();
+    // console.log(sectionID)
+    return <NavLink to={text}><div className={`${sectionID == text ? `${styles.item_content} ${styles.selected}` : styles.item_content}`}>{Icon && <Icon className={styles.icon} />}{text}</div></NavLink>
 }
 export default SideNavigation
